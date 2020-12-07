169a8357b9b4d19cf7319d5a5a6041de73ac68a8
443635978@qq.com <443635978@qq.com>
2020-12-07 17:00:53 +0800

    生成的html文档增加多级分类

--- YApi/exts/yapi-plugin-export-data/controller.js
+++ YApi/exts/yapi-plugin-export-data/controller.js
@@ -17,7 +17,6 @@ class exportController extends baseController {
     this.catModel = yapi.getInst(interfaceCatModel);
     this.interModel = yapi.getInst(interfaceModel);
     this.projectModel = yapi.getInst(projectModel);
-    
   }
 
   async handleListClass(pid, status) {
@@ -34,7 +33,7 @@ class exportController extends baseController {
         newResult.push(item);
       }
     }
-    
+
     return newResult;
   }
 
@@ -87,7 +86,8 @@ class exportController extends baseController {
       }
       ctx.set('Content-Type', 'application/octet-stream');
       const list = await this.handleListClass(pid, status);
-
+      console.log('create html');
+      console.log(list);
       switch (type) {
         case 'markdown': {
           tp = await createMarkdown.bind(this)(list, false);
@@ -113,6 +113,8 @@ class exportController extends baseController {
     }
 
     async function createHtml(list) {
+      console.log('list');
+      console.log(list);
       let md = await createMarkdown.bind(this)(list, true);
       let markdown = markdownIt({ html: true, breaks: true });
       markdown.use(markdownItAnchor); // Optional, but makes sense as you really want to link to something
@@ -133,18 +135,29 @@ class exportController extends baseController {
         }
       );
 
-      return createHtml5(left || '', content);
+      return createHtml5(left || '', content, list);
     }
 
-    function createHtml5(left, tp) {
+    function createHtml5(left, tp, list) {
       //html5模板
       let html = `<!DOCTYPE html>
       <html>
       <head>
       <title>${curProject.name}</title>
+      <script src="https://lib.baomitu.com/jquery/3.5.1/jquery.min.js"></script>
+      <script src="https://lib.baomitu.com/zTree.v3/3.5.42/js/jquery.ztree.core.min.js"></script>
+      <link rel="stylesheet" href="https://lib.baomitu.com/zTree.v3/3.5.42/css/zTreeStyle/zTreeStyle.min.css" >
       <meta charset="utf-8" />
       ${defaultTheme}
       </head>
+      <style>
+        .ztree li {
+          line-height: 200%!important;
+        }
+        .ztree a span{
+          font-size: 14px;
+        }
+      </style>
       <body>
         <div class="m-header">
           <a href="#" style="display: inherit;"><svg class="svg" width="32px" height="32px" viewBox="0 0 64 64" version="1.1"><title>Icon</title><desc>Created with Sketch.</desc><defs><linearGradient x1="50%" y1="0%" x2="50%" y2="100%" id="linearGradient-1"><stop stop-color="#FFFFFF" offset="0%"></stop><stop stop-color="#F2F2F2" offset="100%"></stop></linearGradient><circle id="path-2" cx="31.9988602" cy="31.9988602" r="2.92886048"></circle><filter x="-85.4%" y="-68.3%" width="270.7%" height="270.7%" filterUnits="objectBoundingBox" id="filter-3"><feOffset dx="0" dy="1" in="SourceAlpha" result="shadowOffsetOuter1"></feOffset><feGaussianBlur stdDeviation="1.5" in="shadowOffsetOuter1" result="shadowBlurOuter1"></feGaussianBlur><feColorMatrix values="0 0 0 0 0   0 0 0 0 0   0 0 0 0 0  0 0 0 0.159703351 0" type="matrix" in="shadowBlurOuter1"></feColorMatrix></filter></defs><g id="首页" stroke="none" stroke-width="1" fill="none" fill-rule="evenodd"><g id="大屏幕"><g id="Icon"><circle id="Oval-1" fill="url(#linearGradient-1)" cx="32" cy="32" r="32"></circle><path d="M36.7078009,31.8054514 L36.7078009,51.7110548 C36.7078009,54.2844537 34.6258634,56.3695395 32.0579205,56.3695395 C29.4899777,56.3695395 27.4099998,54.0704461 27.4099998,51.7941246 L27.4099998,31.8061972 C27.4099998,29.528395 29.4909575,27.218453 32.0589004,27.230043 C34.6268432,27.241633 36.7078009,29.528395 36.7078009,31.8054514 Z" id="blue" fill="#2359F1" fill-rule="nonzero"></path><path d="M45.2586091,17.1026914 C45.2586091,17.1026914 45.5657231,34.0524383 45.2345291,37.01141 C44.9033351,39.9703817 43.1767091,41.6667796 40.6088126,41.6667796 C38.040916,41.6667796 35.9609757,39.3676862 35.9609757,37.0913646 L35.9609757,17.1034372 C35.9609757,14.825635 38.0418959,12.515693 40.6097924,12.527283 C43.177689,12.538873 45.2586091,14.825635 45.2586091,17.1026914 Z" id="green" fill="#57CF27" fill-rule="nonzero" transform="translate(40.674608, 27.097010) rotate(60.000000) translate(-40.674608, -27.097010) "></path><path d="M28.0410158,17.0465598 L28.0410158,36.9521632 C28.0410158,39.525562 25.9591158,41.6106479 23.3912193,41.6106479 C20.8233227,41.6106479 18.7433824,39.3115545 18.7433824,37.035233 L18.7433824,17.0473055 C18.7433824,14.7695034 20.8243026,12.4595614 23.3921991,12.4711513 C25.9600956,12.4827413 28.0410158,14.7695034 28.0410158,17.0465598 Z" id="red" fill="#FF561B" fill-rule="nonzero" transform="translate(23.392199, 27.040878) rotate(-60.000000) translate(-23.392199, -27.040878) "></path><g id="inner-round"><use fill="black" fill-opacity="1" filter="url(#filter-3)" xmlns:xlink="http://www.w3.org/1999/xlink" xlink:href="#path-2"></use><use fill="#F7F7F7" fill-rule="evenodd" xmlns:xlink="http://www.w3.org/1999/xlink" xlink:href="#path-2"></use></g></g></g></g></svg></a>
@@ -154,7 +167,9 @@ class exportController extends baseController {
           </div>
         </div>
         <div class="g-doc">
-          ${left}
+          <div class="table-of-contents">
+            <div class="ztree" id="tree"></div>
+          </div>
           <div id="right" class="content-right">
           ${tp}
             <footer class="m-footer">
@@ -162,7 +177,45 @@ class exportController extends baseController {
             </footer>
           </div>
         </div>
-      </body>
+        <script>
+          const list = ${JSON.stringify(list)};
+          const loop = (list) => {
+            list.forEach((item) => {
+              item.isParent = true;
+              if(!item.name) {
+                item.isParent = false;
+                item.name = item.title
+              }
+              if(item.list && item.list.length > 0) {
+                item.children = item.list;
+                loop(item.list);
+              }
+            });
+          }
+          loop(list);
+          console.log('list', list);
+          var setting = {
+            data: {
+              simpleData: {
+                enable: false
+              }
+            },
+            callback: {
+              onClick: (event, treeId, treeNode) => {
+                const id = $('#' + treeNode.name).parents('h2')[0].id;
+                const a = document.createElement('a');
+                a.href = '#' + id;
+                a.target = '_self'
+                a.click();
+              }
+            }
+          };
+          $(document).ready(function(){
+            const tree = $.fn.zTree.init($("#tree"), setting, list);
+            tree.expandAll(true);
+          });
+        </script>
+        </body>
       </html>
       `;
       return html;
