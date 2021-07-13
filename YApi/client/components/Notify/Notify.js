5e4f4b3c3a85abdd25d6ef2d5cd121ff19b863e8
张杨 <443635978@qq.com>
2021-07-13 15:02:34 +0800

    移除获取最新版本号功能

--- YApi/client/components/Notify/Notify.js
+++ YApi/client/components/Notify/Notify.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import axios from 'axios';
+// import axios from 'axios';
 import { Alert, message } from 'antd';
 
 export default class Notify extends Component {
